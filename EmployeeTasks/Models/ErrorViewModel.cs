d2a1c47ddea895a1014d5a9bd763d78642d165f5
nedrett <nedret88@gmail.com>
2023-03-09 17:57:23 +0200

    Add project files.

---
+++ EmployeeTasks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeTasks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
